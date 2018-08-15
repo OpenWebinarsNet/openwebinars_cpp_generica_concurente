e63b502f57ceb58bbe0b66283f019ab9df439377
netwave <3danimanimal@gmail.com>
2018-08-15 11:07:44 -0700

    added more and better examples lack of better prints

--- macros/main.cpp
+++ macros/main.cpp
@@ -3,7 +3,7 @@
 
 #include <iostream>
 #include <string>
-#include "macro_header.h"
+#include "header_example.hpp"
 
 //macro define
 #define MY_FST_MACRO "Hello world"
@@ -11,7 +11,18 @@
 
 //macro redifine
 #define MY_REDEFINED_MACRO(the_variable) #the_variable
-#define MY_REDEFINED_MACRO(the_variable) #the_variable#the_variable
+#define MY_REDEFINED_MACRO(the_variable, other_variable) (#the_variable#other_variable)
+
+//# operator
+#define INIT_10(a) static int my_var_##a = 10;
+
+INIT_10(a)
+INIT_10(b)
+
+//undef
+#undef INIT_10
+
+//INIT_10(c) // this will now work, since we just did undef it in the previours line
 
 
 //macros are non-recursive
@@ -25,14 +36,18 @@ int main() {
     //macro substitution
     std::cout << MY_FST_MACRO << std::endl;
 
+    //macro concat
+    std::cout << my_var_a << std::endl;
+    std::cout << my_var_b << std::endl;
+
     //macro redefinition
-    std::cout << MY_REDEFINED_MACRO(10) << std::endl;
+    //std::cout << MY_REDEFINED_MACRO(10) << std::endl; doesnt work, is redifined
+    std::cout << MY_REDEFINED_MACRO(10, 10) << std::endl;
 
     //macro recursive error, doesn't work
     //std::cout << FAC(10) << std::endl;
 
     //fun with macros...what is the output
-
     std::cout << SQUARE(10)  << std::endl; //10*10 = 100
     std::cout << SQUARE(5+2) << std::endl; //7*7 = 49? or 5+2*5+2?
 
