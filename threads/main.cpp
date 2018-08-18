190aeb9006fe6476e00dd5f63d36918214cb4602
netwave <3danimanimal@gmail.com>
2018-08-18 16:46:21 +0200

    added detached example, formatted for more readeable main function

--- threads/main.cpp
+++ threads/main.cpp
@@ -62,29 +62,59 @@ void t_print_example_with_atomic()
     }
 }
 
+void t_detach_example()
+{
+    std::atomic_bool kill_flag(true);
+    std::thread t
+    (
+        [&kill_flag]()
+        {
+            while(kill_flag)
+            {
+                std::this_thread::sleep_for(std::chrono::seconds(3));
+                std::cout << "Detached thread " << std::this_thread::get_id() << std::endl;
+            }
+            std::cout << "Exit detached thread " << std::this_thread::get_id() << std::endl;
+        }
+    );
+    t.detach();
+    for (int i = 0; i < 10; ++i) {
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+        std::cout << "Main thread " << std::this_thread::get_id() << std::endl;
+    }
+    kill_flag = false;
+    std::cout << "Exit main thread " << std::this_thread::get_id() << std::endl;
+}
+
 int main() {
     std::cout << "Threading print example" << std::endl;
     t_print_example();
+    ////////////////////////////////////////////////////////////////
     std::cout << "Threading with mutex print example" << std::endl;
     t_print_example_with_mutex();
+    ////////////////////////////////////////////////////////////////
     std::cout << "Threading with atomic print example" << std::endl;
     t_print_example_with_atomic();
+    ////////////////////////////////////////////////////////////////
     std::cout << "Threading map example" << std::endl;
     std::cout << "Concurrent threads: " << std::thread::hardware_concurrency() << std::endl;
     ThreadPool pool;
     std::vector<int> args{0,1,2,3,4,5,6,7,8,9,8,7,6,5,4,3,2,1,0};
     auto res = pool.map<std::function<int(int)>, int, int>
     (
-     [](int i)
-     {
-         std::this_thread::sleep_for(std::chrono::seconds(rand()%2));
-         return i +10;
-     },
-     args
+         [](int i)
+         {
+             std::this_thread::sleep_for(std::chrono::seconds(rand()%2));
+             return i +10;
+         },
+         args
     );
     for (int& r: res)
     {
         std::cout << r << std::endl;
     }
+    ////////////////////////////////////////////////////////////////
+    std::cout << "Threading detach example" << std::endl;
+    t_detach_example();
     return 0;
 }
